f10524cc5aa6feacee344d8f706c8b4c3b95dbc9
Eduard Zakarian <antidie99@gmail.com>
2018-02-10 18:53:29 +0000

    Initial Commit

---
+++ DataHandler/Credentials.cs
@@ -0,0 +1,18 @@
+﻿namespace xoxoxo
+{
+    public struct Credentials
+    {
+        public Credentials(string username, string password, string website)
+        {
+            Username = username;
+            Password = password;
+            Website = website;
+        }
+
+        public string Username { get; }
+
+        public string Password { get; }
+
+        public string Website { get; }
+    }
+}
